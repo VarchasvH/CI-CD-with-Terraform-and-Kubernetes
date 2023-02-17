6ab131c1893142c8bd0827918030b506acdef8b0
Varchasv Hoon <100064742+VarchasvH@users.noreply.github.com>
2023-02-17 13:15:05 +0530

    Delete nodeapp directory

--- nodeapp/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express();
-app.get("/", (req, res) =>{
-    res.send("Server is running")
-})
-app.listen(80, () =>{
-    console.log("Server is up")
-})
